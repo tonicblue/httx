e7b0a91c676c5c661df16aee2867651e3df89a08
Matthew Parry-Wilkes <tonicblue@gmail.com>
2024-04-11 12:32:00 +0100

    fixing stupid bug

--- src/templates/dev-server.ts
+++ src/templates/dev-server.ts
@@ -173,8 +173,8 @@ function setupDevServer (port: number, publicDir: string, views: string, viewEng
   console.log('### Creating development server with the following options', { port, publicDir, views, viewEngine, });
 
   const app = Express();
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
+  app.use(Express.json());
+  app.use(Express.urlencoded({ extended: true }));
 
   addRoutesToApp(app);
 
